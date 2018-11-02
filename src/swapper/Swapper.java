27c3ed3472ec867cd25ce02afcf4e597297070cb
Łukasz Zarębski <lkl.zarebski@gmail.com>
2018-11-02 13:59:37 +0100

    done...?

--- src/swapper/Swapper.java
+++ src/swapper/Swapper.java
@@ -2,8 +2,12 @@ package swapper;
 
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Swapper<E> {
+    private final ReentrantLock lock = new ReentrantLock(true);
+    private final Condition setModified = lock.newCondition();
     private HashSet<E> set;
 
     public Swapper() {
@@ -11,6 +15,18 @@ public class Swapper<E> {
     }
 
     public void swap(Collection<E> removed, Collection<E> added) throws InterruptedException {
+        lock.lock();
 
+        while (!set.containsAll(removed)){
+            setModified.await();
+        }
+
+        try {
+            set.removeAll(removed);
+            set.addAll(added);
+        } finally {
+            setModified.signalAll();
+            lock.unlock();
+        }
     }
 }
